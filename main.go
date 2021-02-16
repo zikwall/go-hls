e6083ea97e7c4f2863959c38bb4e37dd50592a69
zikwall <andrey.kapitonov.96@gmail.com>
2021-02-16 18:05:57 +0300

    Add CLI

--- main.go
+++ main.go
@@ -1,30 +1,51 @@
 package main
 
 import (
+	"github.com/urfave/cli/v2"
 	"github.com/zikwall/go-hls/src/http"
 	"github.com/zikwall/go-hls/src/log"
+	"os"
 	"path/filepath"
 )
 
 func main() {
-	absolutePath, err := filepath.Abs("./tmp")
+	application := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "root-file-directory",
+				Usage:   "",
+				EnvVars: []string{"ROOT_FILE_DIRECTORY"},
+				Value:   "./tmp",
+			},
+		},
+	}
 
-	if err != nil {
+	application.Action = func(ctx *cli.Context) error {
+		absolutePath, err := filepath.Abs(ctx.String("root-file-directory"))
+
+		if err != nil {
+			return err
+		}
+
+		httpHandlerProvider := http.HandlerProvider{
+			RootDirectory: absolutePath,
+		}
+
+		go func() {
+			httpHandlerProvider.Serve()
+		}()
+
+		congratulations()
+		waitSystemNotify()
+
+		httpHandlerProvider.Shutdown()
+
+		log.Info("Stopped")
+
+		return nil
+	}
+
+	if err := application.Run(os.Args); err != nil {
 		log.Error(err)
 	}
-
-	httpHandlerProvider := http.HandlerProvider{
-		RootDirectory: absolutePath,
-	}
-
-	go func() {
-		httpHandlerProvider.Serve()
-	}()
-
-	congratulations()
-	waitSystemNotify()
-
-	httpHandlerProvider.Shutdown()
-
-	log.Info("Stopped")
 }
